e4d5a9d478c8eac8a700c91edcc1b75f27003ab1
Amethesh <ametheshlgp@gmail.com>
2025-07-06 19:59:43 +0530

    feat: Google login and base auth style

---
+++ src/components/auth/google-login.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Button } from "@/src/components/ui/button";
+import { createClient } from "@/src/lib/supabase/client";
+import Image from "next/image";
+import React from "react";
+
+const SignInWithGoogleButton = () => {
+  const signInWithGoogle = async () => {
+    const supabase = createClient();
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+  };
+
+  return (
+    <Button
+      type="button"
+      variant="outline"
+      className="p-6 w-full flex items-center rounded-xl gap-2 text-sm text-white bg-black/50 border border-[#313131] focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+      onClick={signInWithGoogle}
+    >
+      <Image
+        src="/images/Google_Favicon_2025.png"
+        alt="Google logo"
+        width={15}
+        height={15}
+      />
+      Continue with Google
+    </Button>
+  );
+};
+
+export default SignInWithGoogleButton;
---
+++ src/components/ui/GlassModal.tsx
@@ -0,0 +1,41 @@
+interface GlassModalProps {
+  width?: number;
+  height?: number;
+  count?: number;
+  className?: string;
+}
+
+const GlassModal: React.FC<GlassModalProps> = ({
+  width = 40,
+  height = 600,
+  count = 18,
+  className = "",
+}) => {
+  const glassItemClasses = [
+    "bg-[linear-gradient(90deg,rgba(22,22,24,0.3)_0%,rgba(0,0,0,0.3)_17%,rgba(26,26,29,0.01)_100%)]",
+    "shadow-[inset_0_2px_4px_rgba(255,255,255,0.16)]",
+    "backdrop-blur-[20px]",
+  ].join(" ");
+
+  const listItems = Array.from({ length: count }, (_, index) => {
+    const isFirst = index === 0;
+    const isLast = index === count - 1;
+
+    const radiusClasses = [
+      isFirst ? "rounded-l-[26px]" : "",
+      isLast ? "rounded-r-[26px]" : "",
+    ].join(" ");
+
+    return (
+      <li
+        key={index}
+        className={`${glassItemClasses} ${radiusClasses}`}
+        style={{ width: `${width}px`, height: `${height}px` }}
+      ></li>
+    );
+  });
+
+  return <ul className={`flex ${className}`}>{listItems}</ul>;
+};
+
+export default GlassModal;
